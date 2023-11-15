77bf5e93f57177a290a2ea0ae3509377dc7139ec
jademon <j2python@gmail.com>
2023-11-15 22:16:13 +0900

    .

--- src/main/java/com/example/JademonLog/entity/post/Post.java
+++ src/main/java/com/example/JademonLog/entity/post/Post.java
@@ -1,4 +1,35 @@
 package com.example.JademonLog.entity.post;
 
+import com.example.JademonLog.entity.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class Post {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String desc;
+
+    private String source;
+
+    private String date;
+
+    @OneToOne
+    private Member writer;
+
+    public Post(String title, String desc, String source, Member writer) {
+//        this.
+    }
+
+
 }
